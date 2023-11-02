6b4fb6ac78f41ae4e752a83cc1cb644673f32c42
Landon Swavey <s541663@nwmissouri.edu>
2023-11-01 20:04:54 -0500

    Added choose

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var guitarsRouter = require('./routes/guitars');
 var boardRouter = require('./routes/board');
+var chooseRouter = require('./routes/choose');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/guitars', guitarsRouter);
+app.use('/choose', chooseRouter);
 app.use('/board', (req, res, next) => {
   const query = req.query;
   res.render('board', { title: 'Board', query });
