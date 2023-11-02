6941d516644e42167d94fc0c8b67a54ab47227f2
Landon Swavey <s541663@nwmissouri.edu>
2023-11-01 19:14:43 -0500

    Finished part 7

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var guitarsRouter = require('./routes/guitars');
+var boardRouter = require('./routes/board');
 
 var app = express();
 
@@ -23,6 +24,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/guitars', guitarsRouter);
+app.use('/board', (req, res) => {
+  let query = req.query;
+  console.log(`rows ${query.rows}`);
+  console.log(`cols ${query.cols}`);
+  res.render('board', { title:'Board', query});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
